52dfc26a7dc89a7a901a1e297e30051b87b77e39
Riyagrrm <120349265+Riyagrrm@users.noreply.github.com>
2024-04-11 17:43:45 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 left_x = ""
 right_x = ""
+difference=""
 function preload()
 {}
 function setup()
@@ -19,11 +20,18 @@ function modelLoaded()
 function draw()
 {
     background("#B6FFBB")
+    textSize('20px')
+    fill('black')
+    text('Hello', 50, 300)
 }
 function gotPoses(results)
 {
     if (results.length > 0)
     {
         console.log(results)
+        left_x = results[0].pose.leftWrist.x
+        right_x = results[0].pose.rightWrist.x
+        difference = left_x - right_x
+        textSize(difference)
     }
 }
